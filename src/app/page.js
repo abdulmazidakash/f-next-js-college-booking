19b2b18b166f10ede648cecca6360ac406789784
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-07-10 15:20:32 +0600

    install daisy ui, next auth, react icons and create navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col items-center justify-center min-h-screen text-4xl font-bold  border-2 border-green-500 bg-section-bg">
-      Hello World!
-  
+    <div>
+      
     </div>
     </>
     
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,80 @@
+'use client';
+
+// import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaSchoolFlag } from "react-icons/fa6";
+
+export default function Navbar() {
+//   const { data: session, status } = useSession();
+//   console.log('navbar session --->', session);
+// const {status} = useSession();
+const status = 'authenticated';
+  const navMenu = () => (
+    <>
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/about">About</Link></li>
+      <li><Link href="/services">Services</Link></li>
+      <li><Link href="/blog">Blog</Link></li>
+      <li><Link href="/my-bookings">My Bookings</Link></li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-navbar-bg shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"/>
+            </svg>
+          </div>
+          <ul tabIndex={0}
+              className="menu font-semibold menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            {navMenu()}
+          </ul>
+        </div>
+        <Link href="/" className="btn btn-ghost text-button-bg text-2xl">
+		<FaSchoolFlag />
+        </Link>
+      </div>
+
+      <div className="navbar-center hidden lg:flex font-semibold">
+        <ul className="menu menu-horizontal px-1">
+          {navMenu()}
+        </ul>
+      </div>
+
+      <div className="navbar-end">
+        {status === 'loading' ? (
+          <span className="loading loading-spinner loading-md"></span>
+        ) : status === 'authenticated' ? (
+          <div className="flex items-center list-none gap-2">
+            <li>
+              <Image
+                src={ true || '/assets/default-avatar.png'}
+                width={40}
+                height={40}
+                alt="user-logo"
+                className="rounded-full"
+                priority
+              />
+            </li>
+            <li 
+			// onClick={() => signOut()} 
+			className="btn btn-error mr-2">Logout</li>
+          </div>
+        ) : (
+          <>
+            {/* <Link href="/register" className="btn btn-error btn-soft">Register</Link> */}
+            <Link href="/login" className="btn btn-warning btn-soft mr-2">Login</Link>
+          </>
+        )}
+        <a className="btn btn-outline">Appointment</a>
+      </div>
+    </div>
+  );
+}
