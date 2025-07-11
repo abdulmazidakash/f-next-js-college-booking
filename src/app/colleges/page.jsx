d981dbb5f8adedd6065c25a4d4ba0daf2d96fc2a
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-07-11 18:02:09 +0600

    fix: colleges page serversideprops error fix

--- src/app/colleges/page.jsx
+++ src/app/colleges/page.jsx
@@ -1,7 +1,15 @@
 import CollegeCard from "@/components/CollegeCard";
 import dbConnect, { collectionNameObject } from "@/lib/dbConnect";
 
-export default function Colleges({ colleges }) {
+export default async function Colleges() {
+  let colleges = [];
+  try {
+    const serviceCollection = dbConnect(collectionNameObject.collegeCollection);
+    colleges = await serviceCollection.find({}).toArray();
+  } catch (error) {
+    console.error("college data fetch error:", error);
+  }
+
   return (
     <div className="container mx-auto px-4 py-6">
       <h1 className="text-3xl font-bold mb-4">All Colleges</h1>
@@ -12,10 +20,4 @@ export default function Colleges({ colleges }) {
       </div>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const serviceCollection = dbConnect(collectionNameObject.collegeCollection);
-  const colleges = await serviceCollection.find({}).toArray();
-  return { props: { colleges: JSON.parse(JSON.stringify(colleges)) } };
 }
