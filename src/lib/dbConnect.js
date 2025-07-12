6f677c30945ba4ef92b3436bc2b3c504c7fd9c40
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-07-12 19:24:04 +0600

    dbconnect and env file edit

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,25 +1,3 @@
-// import { MongoClient, ServerApiVersion } from 'mongodb';
-
-// export const collectionNameObject = {
-//     collegeCollection: 'college_name', 
-//     usersCollection: 'college_user', 
-//     admissionCollection: 'admissions', 
-//     reviewsCollection: 'reviews', 
-// };
-
-// export default function dbConnect(collectionName) {
-// 	const uri = process.env.MONGODB_URI;
-// 	// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// 	const client = new MongoClient(uri, {
-// 	  serverApi: {
-// 		version: ServerApiVersion.v1,
-// 		strict: true,
-// 		deprecationErrors: true,
-// 	  }
-// 	});
-// 	return client.db(process.env.DB_NAME).collection(collectionName);
-// }
-
 // lib/dbConnect.js
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
@@ -34,7 +12,7 @@ export const collectionNameObject = {
 };
 
 export default async function dbConnect(collectionName) {
-    const uri = process.env.MONGODB_URI;
+    const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
     const dbName = process.env.DB_NAME;
 
     if (!uri) {
