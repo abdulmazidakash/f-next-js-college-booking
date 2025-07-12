372291119277d1198e76a5bc55a5ade03867dfdf
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-07-12 14:57:28 +0600

    fix: register user error fix

--- src/app/actions/auth/registerUser.js
+++ src/app/actions/auth/registerUser.js
@@ -1,14 +1,16 @@
-//app/actions/auth/registerUser.js
+// app/actions/auth/registerUser.js
 'use server';
 import bcrypt from 'bcrypt';
 import dbConnect, { collectionNameObject } from "@/lib/dbConnect";
 
 export const registerUser = async(payload)=>{
-	const userCollection = dbConnect(collectionNameObject.usersCollection);
+	// Await dbConnect call
+	const userCollection = await dbConnect(collectionNameObject.usersCollection);
+	
 	// validation 
 	const { email, password } = payload;
 	if(!email || !password){
-		return { success: false, message: "Email and password are required." };
+		return { acknowledged: false, message: "Email and password are required." };
 	}
 
 	const user = await userCollection.findOne({ email: payload.email});
@@ -17,8 +19,10 @@ export const registerUser = async(payload)=>{
 		const hashPassword = await bcrypt.hash(password, 10);
 		payload.password = hashPassword; // Store hashed password
 		const result = await userCollection.insertOne(payload);
-		result.insertedId = result.insertedId.toString();
+		
+		// Ensure insertedId is returned as a string for consistency
+		result.insertedId = result.insertedId.toString(); 
 		return result;
 	}
-	return { success: false, message: "User already exists." };
+	return { acknowledged: false, message: "User already exists." };
 }
